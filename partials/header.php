903cbf567fc3a6eeb81bd3c1f33def4bec265ef9
GutsGusta <gustavonovaes623@gmail.com>
2026-08-27 15:24:49 -0300

    comecei o header

---
+++ partials/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <a href=""><img src=""></a><!-- LOGO -->
+        <!-- BARRA DE PESQUISA -->
+        <a href=""><img src=""></a><!-- CARRINHO -->
+        <a href=""><img src=""></a><!-- LOGIN -->
+        <nav>
+            <a href="">Categorias</a>
+            <div>
+                <a href="">Mais Vendidos</a>
+                <a href="">Autores</a>
+                <a href="">Promoções</a>
+                <a href="">E-book</a>
+            </div>
+        </nav>
+    </header>
+
+</body>
+</html>
