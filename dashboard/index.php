317bcd057eed02fa69e73a3b5939ff67a663412d
grigorinaldi <grigorinaldi@gmail.com>
2026-09-10 16:32:17 -0300

    conteúdo dentro dos arquivos 'dashboard.css','sidebar.php','produto.php','index.php', 'futurotecaDB.sql

---
+++ dashboard/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Dashboard | Futuroteca</title>
+
+    <link rel="stylesheet" href="../css/dashboard.css">
+</head>
+
+<body>
+
+    <?php include 'partials/sidebar.php'; ?>
+
+    <main class="conteudo">
+
+        <h1>Dashboard</h1>
+        <p>Bem-vindo(a) ao painel administrativo da Futuroteca!</p>
+
+    </main>
+
+</body>
+
+</html>
