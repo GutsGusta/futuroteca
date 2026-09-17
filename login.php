857842b6f586445a014eeb4660195f3792bb8597
GutsGusta <gustavonovaes623@gmail.com>
2026-09-17 16:19:58 -0300

    Estilizei o login

--- login.php
+++ login.php
@@ -2,21 +2,31 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/header-footer.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 
 <body>
-    <form class="login-card">      
+    <?php
+        require_once "partials/header.php";
+    ?>
+    <main>
+        <h1>Seja Bem-Vindo de Volta!</h1>
+        <form class="login-card">      
             <h1>Realize seu Login</h1>
-                <input type="text" placeholder="Nome Completo" required>
-
-                <input type="email" placeholder="Email" required>
-
-                <input type="password" placeholder="Senha" required>
-
-                <button type="submit">Login</button>
-    </form>
+            <label>
+                <p>Email</p>
+                <input type="email" required>
+            </label>
+            <label>
+                <p>Senha</p>
+                <input type="password" required>
+            </label>
+            <a href="">Não possui conta? Cadastra-se aqui!</a>
+            <button type="submit">Login</button>
+        </form>
+    </main>
 </body>
 </html>
